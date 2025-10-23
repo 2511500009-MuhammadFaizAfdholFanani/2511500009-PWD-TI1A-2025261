a7785ac0388819d2ca118907efa28fc3686a2add
Muhammad Faiz Afdhol Fanani <2511500009@mahasiswa.atmaluhur.ac.id>
2025-10-23 20:20:58 +0700

    menambah teks discript.js

--- pertemuan-05/script.js
+++ pertemuan-05/script.js
@@ -2,4 +2,4 @@ document.getElementById("menuToggle").addEventListener("click", function () {
     document.querySelector("nav").classList.toggle("active");
 });
 
-document.write("<h2>Halo Dunia</h2>");
+alert("Halo, Selamat Datang!");
